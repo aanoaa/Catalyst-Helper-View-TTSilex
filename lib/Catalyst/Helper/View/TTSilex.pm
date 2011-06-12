5f7a946b647f286242b37eb1a252db2c5efed6a9
Hyungsuk Hong <aanoaa@gmail.com>
2011-06-13 03:04:51 +0900

    initial commit

---
+++ lib/Catalyst/Helper/View/TTSilex.pm
@@ -0,0 +1,17 @@
+package Catalyst::Helper::View::TTSilex;
+
+# ABSTRACT: Catalyst::Helper::View::TTSilex
+use strict;
+use warnings;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
+
+=cut
+
+1;
