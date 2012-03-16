5bfde7f55f297f477cbc323f6cfe06839dc49b8e
Hyungsuk Hong <aanoaa@gmail.com>
2012-03-16 17:28:17 +0900

    using twitter-bootstrap

--- share/root/static/scripts/bundle.js
+++ share/root/static/scripts/bundle.js
@@ -1,5 +1,5 @@
 
-  define(["jquery", "header"], function($) {
+  define(["jquery", "header", "lib/bootstrap-alert", "lib/bootstrap-button", "lib/bootstrap-carousel", "lib/bootstrap-collapse", "lib/bootstrap-dropdown", "lib/bootstrap-modal", "lib/bootstrap-scrollspy", "lib/bootstrap-tab", "lib/bootstrap-tooltip", "lib/bootstrap-popover", "lib/bootstrap-transition", "lib/bootstrap-typeahead"], function($) {
     return $(document).ready(function() {
       return console.log('bundle');
     });
